9ac7651ba064df2c91e60d90d5b50238e1985467
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2018-07-05 22:45:30 -0300

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 setup(
     name='django_brfied',
-    packages=['django_brfied', 'python_brfied', ],
+    packages=['django_brfied', 'django_brfied/migrations', 'django_brfied/management/commands', ],
     package_dir={'django_brfied': 'django_brfied'},
-    package_data={'django_brfied': ['django_brfied/static/js/*', 'django_brfied/*', 'django_brfied/migrations/*', 'django_brfied/management/commands/*'],},
-    version='0.4.5',
-    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.4.5',
+    # package_data={'django_brfied': ['django_brfied/static/js/*', 'django_brfied/*', 'django_brfied/migrations/*', 'django_brfied/management/commands/*'],},
+    version='0.5.0',
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.5.0',
     description='Django Application specific brazilian fields types',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
